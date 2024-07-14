28bda26b643b7ea0137c9c943ea933973beada5d
= <muthuannamalai@example.com>
2024-07-14 15:26:20 +0100

    now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import Body from './components/Body';
 import './components/app.css';
+import './components/mobile.css'
 export default function App() {
     return (
         <div className="body">
--- src/components/section/waitlist.jsx
+++ src/components/section/waitlist.jsx
@@ -2,7 +2,7 @@ function Waitlist() {
     return (
         <div className="thewaitlist">
             <div className="thewait">
-                <h1>Join the Vois Waitlis</h1>
+                <h1>Join the Vois Waitlist</h1>
                 <span>Get ready for the highly anticipated launch of Vois! We're excited to bring you an innovative platform that will revolutionize your communication experience. By joining our waitlist, you'll be the first to know about our official launch and receive exclusive early access to Vois.</span>
                 <div className="butn">
                     <button>Join waitlist</button>
